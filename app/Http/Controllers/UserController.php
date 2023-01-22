d5d3b3ea008ac3bd29ef0195be64991f89f3fc14
Gabriel Victor Raymundo <bieelvii13@gmail.com>
2023-01-22 14:56:55 -0300

    Add ValidationException

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Entities\User;
 use App\Exceptions\PasswordException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use LaravelDoctrine\ORM\Facades\EntityManager;
 
 class UserController extends Controller
@@ -43,6 +44,8 @@ class UserController extends Controller
             $message = "User created with success!";                        
             return back()->with('msg', $message);
         } catch (PasswordException $e) {  
+            return back()->with('msgError', $e->getMessage());
+        } catch (ValidationException $e) {  
             return back()->with('msgError', $e->getMessage());
         } catch (\Throwable $e) {
             $message = "Something wrong happened! Contact an administrator";
