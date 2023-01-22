591847f62fe1aeea49c0f74b9c5111ff0467e184
Gabriel Victor Raymundo <bieelvii13@gmail.com>
2023-01-22 15:18:44 -0300

    refactored a register user and a make a components link and script

--- resources/views/register/user.blade.php
+++ resources/views/register/user.blade.php
@@ -1,38 +1,48 @@
-@extends('home')
-
-@section('content')
-
-<main style="height: 100vh">
-    <section class="d-flex justify-content-center align-items-center">
-        <div class="w-25 bg-light rounded px-5 py-5">
-            <form action="{{ route('user.store') }}" method="post">
-                @csrf
-                <div class="mb-3">
-                    <label for="nickname" class="form-label">Name or nickname</label>
-                    <input type="nickname" class="form-control" id="nickname" name="nickname" placeholder="bieelvi" required>
-                </div>
-        
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
-                </div>
-        
-                <div class="mb-3">
-                    <label for="password" class="form-label">Your password</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="your password" required>
-                </div>
-        
-                <div class="mb-3">
-                    <label for="repeatPassword" class="form-label">Repeat your password</label>
-                    <input type="password" class="form-control" id="repeatPassword" name="repeatPassword" placeholder="repeat your password" required>
-                </div>
-        
-                <div>
-                    <button type="submit" class="btn btn-primary mb-3">Submit identity</button>
-                </div>
-            </form>   
-        </div>
-    </section>
-</main>
-    
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @include('components.link')
+    <title>Mitology - Register</title>
+</head>
+<body class="bg-dark">
+    <main class="d-flex flex-column justify-content-center align-items-center" style="height: 100vh">
+        <section>
+            <div class="text-white text-center">
+                <h1>MITOLOGY API</h1>
+            </div>
+            <div class="bg-light rounded px-5 py-5">
+                <form action="{{ route('user.store') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="nickname" class="form-label">Name or nickname</label>
+                        <input type="nickname" class="form-control" id="nickname" name="nickname" placeholder="bieelvi" required>
+                    </div>
+            
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
+                    </div>
+            
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Your password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="your password" required>
+                    </div>
+            
+                    <div class="mb-3">
+                        <label for="repeatPassword" class="form-label">Repeat your password</label>
+                        <input type="password" class="form-control" id="repeatPassword" name="repeatPassword" placeholder="repeat your password" required>
+                    </div>
+            
+                    <div>
+                        <button type="submit" class="btn btn-primary mb-3">Submit identity</button>
+                    </div>
+                </form>   
+            </div>
+        </section>
+    </main>    
+</body>
+@include('components.script')
+</html>
