2e4aca36a0667677fcbf0d8cbe1df69dcce95692
Gabriel Victor Raymundo <bieelvii13@gmail.com>
2023-01-29 00:23:49 -0300

    Add logo. Fixed mobile version

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,33 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @include('components.link')
+    @include('components.script')
     <title>{{ $titlePage }}</title>
 </head>
 
 <body class="bg-dark">
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">LOGO</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" href="#">API</a>
-                    </div>
-                </div>
+        <nav class="container-fluid navbar navbar-expand-lg navbar-dark">
+            <a class="navbar-brand" href="{{ route('home.index') }}">
+                <img src="{{ asset('img/logo-header.png') }}" width="45px" alt="Header logo">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <a class="btn text-white" href="{{ route('profile.index') }}">Profile</a> 
+
                 <div class="navbar-nav">
                     @if (Session::has('logged_user'))
-                        <form action="{{ url('/logout') }}" method="post">
+                        <form action="{{ route('logout') }}" method="post">
                             @csrf
                             <input type="submit" class="btn text-white" value="Logout">
                         </form>    
-                    @else                        
-                        <form action="{{ url('/get') }}" method="post">
-                            @csrf
-                            <input type="submit" class="btn text-white" value="Login">
-                        </form>    
+                    @else    
+                        <a class="btn text-white" href="{{ route('login.index') }}">Login</a>
                     @endif
                 </div>
             </div>
